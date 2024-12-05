c38d65235340e4f43c1f6d5ceaee3a70820beeb4
careyarmst <careyarmst@earthlink.net>
2024-12-05 18:01:02 -0500

    first commit

---
+++ routes/blogs.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const Blog = require("../models/blog");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", {
+        title: "All Blogs",
+        blogs: result
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/create", (req, res) => {
+  res.render("create", { title: "Create" });
+});
+
+router.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then(() => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(() => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
